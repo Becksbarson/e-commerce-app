7e851409b5f09df8d246da14316d14124bd11e05
Barson Rebecca Hanu <barsonrebecca3@gmail.com>
2021-10-24 15:36:32 +0000

    Screens

--- App.js
+++ App.js
@@ -4,9 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <MainNavigator.Navigator
+          screenOptions={{ headerShown: true ,  }}
+          // initialRouteName=""
+        >
+          <MainNavigator.Screen name="Login" component={Login} />
+          <MainNavigator.Screen name="Home" component={Home} />
+         
+        </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
