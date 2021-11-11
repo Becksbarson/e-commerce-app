88e80600bad98ce0ab4483458c2fe14495669e0a
Barson Rebecca Hanu <86298589+Becksbarson@users.noreply.github.com>
2021-11-11 07:59:11 +0000

    FirstCard

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { View } from "react-native";
 import { Ionicons, FontAwesome, AntDesign } from "@expo/vector-icons";
+import { Card } from 'antd';
+
+const { Meta } = Card;
 
 export default function Home(params) {
   return (
@@ -25,6 +28,15 @@ export default function Home(params) {
           <AntDesign name="search1" size={20} color="black" />
           <Ionicons name="notifications-outline" size={20} color="black" />
         </View>
+
+        <Card
+    hoverable
+    style={{ width: 240 }}
+    cover={<img alt="example" src="https://uk.millybridal.org/desktop/cache/details/fresh/D020103201.jpg" />}
+  >
+    <Meta title="Lush prom dress for lovely evening" description="Pearl Green Dress" />
+  </Card>
+      
       </View>
     </View>
   );
