7d3728b3cc76bf69bbd991c3a00a228860d575f5
Barson Rebecca Hanu <86298589+Becksbarson@users.noreply.github.com>
2021-11-11 08:36:03 +0000

    Adding Checkout Page

---
+++ screens/Checkout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View } from "react-native";
+import { Ionicons, FontAwesome, AntDesign } from "@expo/vector-icons";
+
+
+export default function Home(params) {
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        paddingTop: 55,
+        paddingHorizontal: 20,
+      }}
+    >
+     
+    </View>
+  );
+}
